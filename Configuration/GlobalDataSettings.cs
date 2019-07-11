70a64f1d24d1a0b8801b39ce8f16a62c5787599e
Josh Griffith <josh@starshipconsulting.com>
2019-07-11 19:44:21 -0400

    commit

--- Configuration/GlobalDataSettings.cs
+++ Configuration/GlobalDataSettings.cs
@@ -1,5 +1,7 @@
 ﻿namespace Starship.Data.Configuration {
     public static class GlobalDataSettings {
         public static string SystemOwnerName = "system";
+
+        public static bool MultiTenant = true;
     }
 }
